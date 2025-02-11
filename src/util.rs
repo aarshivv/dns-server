502010b3de564b55462b13c6c43dd57096feb168
aarshivv <aarshivsingh@gmail.com>
2025-02-11 13:38:19 -0500

    Add more struct

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,154 @@
+use std::net::Ipv4Addr;
+
+pub enum ResultCode {
+    NOERROR = 0,
+    FORMERR = 1,
+    SERVFAIL = 2,
+    NXDOMAIN = 3,
+    NOTIMP = 4,
+    REFUSED = 5,
+}
+
+impl ResultCode {
+    pub fn from_num(num: u8) -> Self {
+        match num {
+            1 => ResultCode::FORMERR,
+            2 => ResultCode::SERVFAIL,
+            3 => ResultCode::NXDOMAIN,
+            4 => ResultCode::NOTIMP,
+            5 => ResultCode::REFUSED,
+            0 | _ => ResultCode::NOERROR,
+        }
+    }
+}
+
+pub struct DnsHeader {
+    pub id: u16,
+
+    pub recursion_desired: bool,
+    pub truncated_message: bool,
+    pub authoritative_answer: bool,
+    pub opcode: u8,
+    pub response: bool,
+
+    pub rescode: ResultCode,
+    pub checking_disabled: bool,
+    pub authed_data: bool,
+    pub z: bool,
+    pub recursion_available: bool,
+
+    pub questions: u16,
+    pub answers: u16,
+    pub authoritative_entries: u16,
+    pub resource_entries: u16,
+}
+
+impl DnsHeader {
+    pub fn new() -> Self {
+        Self {
+            id: 0,
+            recursion_desired: false,
+            truncated_message: false,
+            authoritative_answer: false,
+            opcode: 0,
+            response: false,
+            rescode: ResultCode::NOERROR,
+            checking_disabled: false,
+            authed_data: false,
+            z: false,
+            recursion_available: false,
+            questions: 0,
+            answers: 0,
+            authoritative_entries: 0,
+            resource_entries: 0,
+        }
+    }
+
+    pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<(), String> {
+        self.id = buffer.read_u16()?;
+
+        let flags = buffer.read_u16()?;
+        let a = (flags >> 8) as u8;
+        let b = (flags & 0xFF) as u8;
+
+        self.recursion_desired = (a & (1 << 0)) > 0;
+        self.truncated_message = (a & (1 << 1)) > 0;
+        self.authoritative_answer = (a & (1 << 2)) > 0;
+        self.opcode = (a >> 3) & 0x0F;
+        self.response = (a & (1 << 7)) > 0;
+
+        self.rescode = ResultCode::from_num(b & 0x0F);
+        self.checking_disabled = (b & (1 << 4)) > 0;
+        self.authed_data = (b & (1 << 5)) > 0;
+        self.z = (b & (1 << 6)) > 0;
+        self.recursion_available = (b & (1 << 7)) > 0;
+
+        self.questions = buffer.read_u16()?;
+        self.answers = buffer.read_u16()?;
+        self.authoritative_entries = buffer.read_u16()?;
+        self.resource_entries = buffer.read_u16()?;
+
+        Ok(())
+    }
+}
+
+pub enum QueryType {
+    UNKNOWN(u16),
+    A, //1
+}
+
+impl QueryType {
+    pub fn to_num(&self) -> u16 {
+        match *self {
+            Self::UNKNOWN(x) => x,
+            Self::A => 1
+        }
+    }
+
+    pub fn from_num(num: u16) -> Self {
+        match num {
+            1 => Self::A,
+            other => QueryType::UNKNOWN(other)
+        }
+    }
+}
+
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType
+}
+
+impl DnsQuestion {
+    pub fn new(name: String, qtype: QueryType) -> Self {
+        Self { name: name, qtype: qtype }
+    }
+
+    pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<(), String> {
+        buffer.read_qname(&mut self.name)?;
+        self.qtype = QueryType::from_num(buffer.read_u16()?); // qtype
+        let _ = buffer.read_u16()?; // class
+
+        Ok(())
+    }
+}
+
+pub enum DnsRecord {
+    UNKNOWN {
+        domain: String,
+        qtype: u16,
+        data_len: u16,
+        ttl: u32
+    }, // 0
+    A {
+        domain: String,
+        addr: Ipv4Addr,
+        ttl: u32
+    }, // 1
+}
+
+impl DnsRecord {
+}
+
 pub struct BytePacketBuffer {
     pub buf: [u8; 512],
     pub pos: usize,
@@ -5,7 +156,10 @@ pub struct BytePacketBuffer {
 
 impl BytePacketBuffer {
     pub fn new() -> BytePacketBuffer {
-        Self { buf: [0; 512], pos: 0 }
+        Self {
+            buf: [0; 512],
+            pos: 0,
+        }
     }
 
     fn pos(&self) -> usize {
@@ -26,7 +180,7 @@ impl BytePacketBuffer {
 
     fn read(&mut self) -> Result<u8, String> {
         if self.pos >= 512 {
-            return Err("End of buffer".into())
+            return Err("End of buffer".into());
         }
 
         let res = self.buf[self.pos];
@@ -37,7 +191,7 @@ impl BytePacketBuffer {
 
     fn get(&mut self, pos: usize) -> Result<u8, String> {
         if pos >= 512 {
-            return Err("End of buffer".into())
+            return Err("End of buffer".into());
         }
 
         Ok(self.buf[pos])
@@ -45,7 +199,7 @@ impl BytePacketBuffer {
 
     fn get_range(&mut self, start: usize, len: usize) -> Result<&[u8], String> {
         if start + len >= 512 {
-            return Err("End of buffer".into())
+            return Err("End of buffer".into());
         }
 
         Ok(&self.buf[start..start + len as usize])
@@ -58,7 +212,10 @@ impl BytePacketBuffer {
     }
 
     fn read_u32(&mut self) -> Result<u32, String> {
-        let res = ((self.read()? as u32) << 24) | ((self.read()? as u32) << 16) | ((self.read()? as u32) << 8) | ((self.read()? as u32) << 0);
+        let res = ((self.read()? as u32) << 24)
+            | ((self.read()? as u32) << 16)
+            | ((self.read()? as u32) << 8)
+            | ((self.read()? as u32) << 0);
 
         Ok(res)
     }
@@ -79,7 +236,41 @@ impl BytePacketBuffer {
 
             let len = self.get(pos)?;
 
-            if (len & 0xC0) == 0xC0 {}
+            if (len & 0xC0) == 0xC0 {
+                if !jumped {
+                    self.seek(pos + 2)?;
+                }
+
+                let b2 = self.get(pos + 1)? as u16;
+                let offset = (((len as u16) ^ 0xc0) << 8) | b2;
+                pos = offset as usize;
+
+                jumped = true;
+                jumps_performed += 1;
+
+                continue;
+            } else {
+                pos += 1;
+
+                if len == 0 {
+                    break;
+                }
+
+                outstr.push_str(delim);
+
+                let str_buffer = self.get_range(pos, len as usize)?;
+                outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+
+                delim = ".";
+
+                pos += len as usize;
+            }
         }
+
+        if !jumped {
+            self.seek(pos)?;
+        }
+
+        Ok(())
     }
 }
